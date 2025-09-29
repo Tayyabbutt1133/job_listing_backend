15a7212461c6cb06c3d5a3e225b63a7a895ebbc6
Tayyeb Shahzad <tayyebbutt966@gmail.com>
2025-09-29 09:13:39 +0500

    added default port

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ from db import db
 from routes.job_route import job_bp
 from flask_cors import CORS
 from Scraper.scrape import scrape_actuary_jobs
+import os
 
 def create_app():
-
     app = Flask(__name__)
     CORS(app)
     app.config.from_object(Config)
     db.init_app(app)
     app.register_blueprint(job_bp)
-
     return app
+
 
 app = create_app()
 
@@ -21,3 +21,9 @@ with app.app_context():
     scrape_actuary_jobs()
     db.create_all()
 
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080)),
+        debug=False
+    )
